8a940e707063a160b630216e1058a0fda74271c7
GiantTree <marv.1997@live.de>
2017-12-25 03:04:03 +0100

    Add documentation and order the code better.

--- src/main/java/gtlp/groundmc/customrecipes/CustomRecipes.java
+++ src/main/java/gtlp/groundmc/customrecipes/CustomRecipes.java
@@ -27,35 +27,79 @@ public class CustomRecipes extends JavaPlugin {
      */
     @SuppressWarnings("deprecation")
     private final List<Recipe> recipes = Lists.newArrayList(
-            // 2 Logs to 16 sticks, old logs
+            /*
+            Shapeless recipes:
+             */
+            /*
+             * Shapeless recipe to convert three wheat to bread.
+             * Allows crafting bread without a crafting table.
+             */
+            new ShapelessRecipe(
+                    new NamespacedKey(this, "wheatToBreadShapeless"),
+                    new ItemStack(Material.BREAD, 1)).
+                    addIngredient(3, Material.WHEAT),
+
+            /*
+            Shaped recipes:
+             */
+            /*
+             * 2 Logs to 16 sticks, old logs.
+             * Recipe:
+             * L
+             * L
+             *
+             * Where "L" is a log.
+             */
             new ShapedRecipe(
                     new NamespacedKey(this, "logToStick"),
                     new ItemStack(Material.STICK, 16)).
                     shape("L", "L").
                     setIngredient('L', Material.LOG, WILDCARD),
-            // 2 Logs to 16 sticks, new logs
+            /*
+             * 2 Logs to 16 sticks, new logs.
+             * Recipe:
+             * L
+             * L
+             *
+             * Where "L" is a log.
+             */
             new ShapedRecipe(
                     new NamespacedKey(this, "log2ToStick"),
                     new ItemStack(Material.STICK, 16)).
                     shape("L", "L").
                     setIngredient('L', Material.LOG_2, WILDCARD),
-            // Logs in the shape of planks for a chest to 4 chests, old logs
+            /*
+             * Logs in the shape of planks for a chest to 4 chests, old logs.
+             * Recipe:
+             * L L L
+             * L   L
+             * L L L
+             *
+             * Where "L" is a log.
+             */
             new ShapedRecipe(
                     new NamespacedKey(this, "logToChest"),
                     new ItemStack(Material.CHEST, 4)).
                     shape("LLL", "L L", "LLL").
                     setIngredient('L', Material.LOG, WILDCARD),
-            // Logs in the shape of planks for a chest to 4 chests, new logs
+            /*
+             * Logs in the shape of planks for a chest to 4 chests, new logs.
+             * Recipe:
+             * L L L
+             * L   L
+             * L L L
+             *
+             * Where "L" is a log.
+             */
             new ShapedRecipe(
                     new NamespacedKey(this, "log2ToChest"),
                     new ItemStack(Material.CHEST, 4)).
                     shape("LLL", "L L", "LLL").
-                    setIngredient('L', Material.LOG_2, WILDCARD),
-            // Shapeless wheat to bread
-            new ShapelessRecipe(
-                    new NamespacedKey(this, "wheatToBreadShapeless"),
-                    new ItemStack(Material.BREAD, 1)).
-                    addIngredient(3, Material.WHEAT)
+                    setIngredient('L', Material.LOG_2, WILDCARD)
+            /*
+            Furnace Recipes:
+             */
+
     );
 
     @Override
